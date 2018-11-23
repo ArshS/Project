e6e6ff82c98a4362f98188782f9a52241e5fab5d
Arshdeep (Arshdeep) Singh <sina3615@36loki.cs.uleth.ca>
2018-11-23 12:52:14 -0700

    Added listerners and senders

--- client.cpp
+++ client.cpp
@@ -2,6 +2,49 @@
 #include "SocketException.h"
 #include <iostream>
 #include <string>
+
+int main(int argc, int argv[])
+{
+   try{
+      // Replace "localhost" with the hostname
+      // that you're running your server.
+   	  //for data
+      ClientSocket clientListener("localhost", 30000);
+      //for acks and naks
+      ClientSocket clientSender("localhost" , 30001);
+      std::string reply;
+      std::string request;
+      request= "gimmee";
+      // Usually in real applications, the following
+      // will be put into a loop. 
+      clientSender << request;
+      while(request == "gimmee"){
+      try {
+	 	//client_socket << "Test message.";
+
+	 	clientListener >> reply;
+	 	std::string Ack;
+	 	Ack = "ACK";
+	 	clientSender << Ack;
+	 	//clientListener >> reply;
+      }
+      catch(SocketException&){
+      }
+      std::cout << "We received this response from the server:\n\"" << reply << "\"\n";;
+  	}
+   }
+   catch(SocketException& e){
+      std::cout << "Exception was caught:" << e.description() << "\n";
+   }
+
+   return 0;
+}
+
+/*
+#include "ClientSocket.h"
+#include "SocketException.h"
+#include <iostream>
+#include <string>
 //using namespace std;
 
 int main()
@@ -9,7 +52,7 @@ int main()
    try{
       // Replace "localhost" with the hostname
       // that you're running your server.
-      ClientSocket client_socket("localhost", 30000);
+      ClientSocket client_socket("localhost", 30002);
       std::string reply;
       //std::stringstream s;
       // Usually in real applications, the following
@@ -39,3 +82,4 @@ int main()
 
    return 0;
 }
+*/
--- server.cpp
+++ server.cpp
@@ -3,35 +3,102 @@
 #include <string>
 
 using namespace std;
+std::string ack2 = "ACK";   
+ 
+int main(int argc, int argv[])
+{
+
+   std::cout << "running....\n";
+
+   try{
+	      // Create the socket
+   		//data
+	   	ServerSocket serverSender(30000);
+	   	//acks and naks
+	   	ServerSocket serverListener(30001);
+
+
+	    while (true){
+		 ServerSocket data_sock;
+		 ServerSocket ack_sock;
+		 serverSender.accept(data_sock);
+		 serverListener.accept(ack_sock);
+	
+		 // For multiple threading, you need to create
+		 // a new thread here and pass data_sock to it.
+		 // The thread will use data_sock to communicate
+		 std::string request;
+		 ack_sock >> request;
+		 // with the client.
+		 if(request == "gimmee")
+		 {
+		 	cout<<"Got request"<<endl;
+		 	//server.accept(ack_sock);
+		 
+		 try{
+		    while (true){
+		       std::string data ="Hello";
+		       std::string ack;
+		     
+		       data_sock << data;
+		
+		       ack_sock >> ack;
+		       if(ack.compare(ack2) == 0)
+		       {
+		        	//data_sock << data;
+		        	cout<<"GOT ack!"<<endl;
+		       }
+		    }
+		 }
+		 catch(SocketException&){
+		 }
+	      }
+	   } 
+	}
+	catch (SocketException& e){
+	std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
+   }
+  return 0;
+}
+
+/*
+#include "ServerSocket.h"
+#include "SocketException.h"
+#include <string>
+
+using namespace std;
  
 int main(int argc, int argv[])
 {
    std::cout << "running....\n";
    try{
       // Create the socket
-      ServerSocket server(30000);
+     ServerSocket server(30002);
 
-      while (true){
-	 ServerSocket new_sock;
-	 server.accept(new_sock);
+     while (true){
+	 	ServerSocket data_sock;
+	 	server.accept(data_sock);
 
-	 // For multiple threading, you need to create
-	 // a new thread here and pass new_sock to it.
-	 // The thread will use new_sock to communicate
-	 // with the client.
-	 try{
-	    while (true){
-	       string data;
-	       new_sock >> data;
-	       new_sock << data;
-	    }
-	 }
-	 catch(SocketException&){
-	 }
+		 // For multiple threading, you need to create
+		 // a new thread here and pass data_sock to it.
+		 // The thread will use data_sock to communicate
+		 // with the client.
+		try{
+		    while (true){
+		       		string data="poopface";
+		       		data_sock >> data;
+		    	   data_sock << data;
+			    }
+			}
+	 		catch(SocketException&){
+		}
       }
+      //server.closesocket();
    }
    catch (SocketException& e){
       std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
    }
+
   return 0;
 }
+*/
