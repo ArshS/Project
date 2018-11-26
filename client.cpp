ab5276573f7c864b9999d5fd56dac94aadafcdb4
Arshdeep (Arshdeep) Singh <sina3615@36wolverine.cs.uleth.ca>
2018-11-26 13:35:18 -0700

    Added files

--- client.cpp
+++ client.cpp
@@ -6,7 +6,15 @@
 using namespace std;
 string Ack = "ACK";
 
+void receiveTransmission();
+
 int main(int argc, int argv[])
+{
+	receiveTransmission();
+	return 0;
+}
+
+void receiveTransmission()
 {
 	try
 	{
@@ -29,40 +37,4 @@ int main(int argc, int argv[])
 		}
 	}
 	catch(SocketException& e){cout<<"Exception was caught:"<<e.description()<<"\n";}
-	return 0;
-
 }
-/*
-int main(int argc, int argv[])
-{
-   try{
-		// Replace "localhost" with the hostname
-		// that you're running your server.
-			//for data
-		ClientSocket clientListener("localhost", 30000);
-		//for acks and naks
-		ClientSocket clientSender("localhost" , 30001);
-		string reply;
-		string request;
-		request= "gimmee";
-		// Usually in real applications, the following
-		// will be put into a loop. 
-		clientSender << request;
-		clientListener >> reply;
-		clientSender << Ack;
-		while(reply!="EOF"){
-			try {
-		 		//client_socket << "Test message.";
-		  		cout << reply << "\n";
-		 		clientListener >> reply;
-		 		clientSender << Ack;
-		 		//clientListener >> reply;
-		  	}
-		  	catch(SocketException&){}
-		}
-	}
-  	catch(SocketException& e){
-  	std::cout << "Exception was caught:" << e.description() << "\n";}
-  return 0;
-}
-*/
--- server.cpp
+++ server.cpp
@@ -7,13 +7,27 @@
 using namespace std;
 std::string Ack2 = "ACK";   
 
-int main(){	
+void sendTransmission(ifstream& infile);
+vector<string> lineReader(ifstream&);
+
+int main()
+{	
+	ifstream infile;
+	infile.open("/home/sina3615/CPSC3780Project/Project/file.txt");
+   	sendTransmission(infile);
+	return 0;
+}
+
+void sendTransmission(ifstream& infile)
+{
 	std::cout << "running....\n";
+	//ifstream infile;
+   	//infile.open("/home/sina3615/CPSC3780Project/Project/file.txt");
 	ServerSocket data_sock;
 	ServerSocket ack_sock;
-   	ifstream infile;
-   	infile.open("/home/sina3615/CPSC3780Project/Project/file.txt");
-   	string data;
+	vector<string> frames;
+	frames = lineReader(infile);
+	string data;
    	try{
   		//data
    		ServerSocket serverSender(30000);
@@ -30,8 +44,12 @@ int main(){
 	 	// with the client.
    		if(request == "Request"){// "gimmee"){
 			cout<<"Got request"<<endl;
-			while(getline(infile, data)){
-				data += '\n';
+			//while(getline(infile, data)){
+			//for(vector<string>::iterator it = frames.begin(); it != frames.end(); ++it)
+			for(int i = 0; i<frames.size(); i++)
+			{
+				//data += '\n';
+				data = frames[i];
 				try{
 			    //while (true){
 			       //data ="Hello";
@@ -44,7 +62,7 @@ int main(){
 			        if(ack.compare(Ack2) == 0)
 			        {
 			        	//data_sock << data;
-			        	cout<<"GOT ack!"<<endl;
+			        	cout<<"Got ack!"<<endl;
 			        }
 			    }
 			    catch(SocketException&){}
@@ -55,6 +73,32 @@ int main(){
 	}
 	data="EOF";
 	data_sock << data;
-	return 0;
+}
+
+vector<string> lineReader(ifstream& infile)
+{
+	vector<string> frames;
+	string data;
+	int step=0;
+	while(getline(infile, data))
+	{
+		for(int i=0; i<data.length(); i+65)
+		{
+			if((data.length()-i)<64)
+			{
+				frames.push_back(data.substr(i, (data.length()-i)));
+			}
+			else
+			{
+				frames.push_back(data.substr(i,64));
+			}
+		}
+		//data+='\n';
+		//frames.push_back(data.substr(i, 64));
+
+		//step=step+65;
+	}
+
+	return frames;
 }
 
