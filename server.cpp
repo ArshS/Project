977eae0714c3fde4357e2bfbd8576ec99691b182
Arshdeep (Arshdeep) Singh <sina3615@36hawkeye.cs.uleth.ca>
2018-12-01 15:44:11 -0700

    Clean up

--- server.cpp
+++ server.cpp
@@ -6,20 +6,13 @@
 #include <vector>
 #include <iostream>
 #include <pthread.h>
-//#include <thread>
 
 using namespace std;
+
 std::string Ack2 = "ACK";   
 
-struct Arg
-{
- 	ServerSocket dataptr;
- 	ServerSocket ackptr;
-};
-
-//#define NTHREADS 1 
 #define NTHREADS 5
-int p=0;
+int cThread=0;
 
 void *sendTransmission(void *);
 vector<string> frameBuilder(ifstream&);
@@ -28,19 +21,13 @@ string swapParity(string);
 string convert(int);
 vector<string> openFile(string);
 
-//ServerSocket data_sock;
-//ServerSocket ack_sock;
-//data
 ServerSocket serverSender(30000);
-//ack
 ServerSocket serverListener(30001);
-//serverSender.accept(data_sock);
-//		serverListener.accept(ack_sock);
 
 int main()
 {
 	int *x;
-
+	cout<<"Initializing..."<<endl;
 	sendTransmission(x);
 
 	return 0;
@@ -48,97 +35,70 @@ int main()
 
 void *sendTransmission(void *args)
 {
-	//ifstream infile;
-	//infile.open("/home/sina3615/CPSC3780Project/Project/file.txt");
 	
-	std::cout << "running....\n";
-	//Arg a1;
-	
-	//serverSender.accept(data_sock);
-		//serverListener.accept(ack_sock);
+	std::cout << "Running thread: "<<cThread<<endl;
+
 	ServerSocket data_sock;
 	ServerSocket ack_sock;
 
-	//data_sock = &args.dataptr;
-	//ack_sock = &args.ackptr;
-
-	//vector<string> frames;
-	//vector<string> frames2;
-	//frames2 = frameBuilder(infile);
-	//frames = parityGetter(frames2);
-
-	cout<<"before loop"<<endl;
 	string data;
 	while(true)
 	{
-   	try{
-  		//data
-   		//ServerSocket serverSender(30000);
-   		//ack
-		//ServerSocket serverListener(30001);
-		serverSender.accept(data_sock);
-		serverListener.accept(ack_sock);
+	   	try{
+			serverSender.accept(data_sock);
+			serverListener.accept(ack_sock);
 
-	 	// For multiple threading, you need to create
-	 	// a new thread here and pass data_sock to it.
-	 	// The thread will use data_sock to communicate
-		std::string request, file;
-		//ack_sock >> file;
-		cout<<"getting file"<<endl;
-		ack_sock >> file;
-		cout<<"got file"<<endl;
+			std::string request, file;
 
-		pthread_t thread_id[NTHREADS];
-		//int p=0;
-		while(!(p<NTHREADS))
-		{
-			cout<<"waiting for free thread..."<<endl;
-		}
-		if(p<NTHREADS)
-		{
-			pthread_create(&thread_id[p], NULL, &sendTransmission ,NULL);
-			p++;
-		}
+			//cout<<"Getting file..."<<endl;
+			ack_sock >> file;
+			//cout<<"Got file."<<endl;
 
-		vector<string> frames = openFile(file);
-		//send(frames, data_sock, ack_sock, data);
-		for(int i = 0; i<frames.size(); i++)
-		{
-			data = frames[i];
-			try{
-		    	std::string ack;
-		    
-		        data_sock << data;
-		        //arg->data_sock << data; 
-		
-		        ack_sock >> ack;
-		        //arg->ack_sock >> ack;
+			pthread_t thread_id[NTHREADS];
+			while(!(cThread<NTHREADS))
+			{
+				cout<<"Waiting for free thread..."<<endl;
+			}
+			if(cThread<NTHREADS)
+			{
+				pthread_create(&thread_id[cThread], NULL, &sendTransmission ,NULL);
+				cThread++;
+			}
 
-		        if(ack.compare(Ack2) == 0)
-		        {
-		        	//cout<<"Got ack!"<<endl;
-		        }
-		        else if(ack.compare(Ack2)!=0)
-		        {
-		        	while (ack.compare(Ack2)!=0)
-		        	{
-		        		//cout<<"Got NAK! Retransmitting..."<<endl;
-		     			data = swapParity(data);
-		        		data_sock << data;
-		        		//arg->data_sock << data; 
-		       			ack_sock >> ack;
-		       			//arg->ack_sock >> ack;
-		       		}
-		        }
-		    }
-		    catch(SocketException&){}
-		}
-	}catch (SocketException& e){std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";}
-	data="EOF";
-	data_sock << data;
-	p--;
-	//arg->data_sock << data; 
-}
+			vector<string> frames = openFile(file);
+			//sleeping to view the thread id and file request on server side
+			sleep(3);
+			for(int i = 0; i<frames.size(); i++)
+			{
+				data = frames[i];
+				try{
+			    	std::string ack;
+			    
+			        data_sock << data;
+			        ack_sock >> ack;
+
+			        if(ack.compare(Ack2) == 0)
+			        {
+			        	cout<<"Got ACK!"<<endl;
+			        }
+			        else if(ack.compare(Ack2)!=0)
+			        {
+			        	while (ack.compare(Ack2)!=0)
+			        	{
+			        		cout<<"Got NAK! Retransmitting..."<<endl;
+			     			data = swapParity(data);
+			        		data_sock << data;
+			       			ack_sock >> ack;
+			       		}
+			        }
+			    }
+			    catch(SocketException&){}
+			}
+		}catch (SocketException& e){ std::cout << "Exception was caught:" << e.description() << "\nExiting.\n"; }
+		data="EOF";
+		data_sock << data;
+		cThread--;
+	}
 }
 
 vector<string> openFile(string file)
@@ -147,29 +107,31 @@ vector<string> openFile(string file)
 	vector<string> frames2;
 	ifstream infile;
 
-	char filepath[file.size()];
-	for(int i=0; i<file.size(); i++)
-		filepath[i]=file[i];
-
-	if(file == "/home/sina3615/CPSC3780Project/Project/file.txt")
+	if(file == "file.txt")
    	{
 		cout<<"Got request for file 1"<<endl;
-		infile.open(filepath);
-		// /home/sina3615/CPSC3780Project/Project/file.txt
+		infile.open("file.txt");
 		frames2 = frameBuilder(infile);
 		frames = parityGetter(frames2);
 	}
-	else if(file == "/home/sina3615/CPSC3780Project/Project/big.txt")
+	else if(file == "big.txt")
    	{
-		cout<<"Got request for file 2"<<endl;
-		infile.open(filepath);
+		cout<<"Got request for big file"<<endl;
+		infile.open("big.txt");
 		frames2 = frameBuilder(infile);
 		frames = parityGetter(frames2);
 	}
-	else//(file == "/home/sina3615/CPSC3780Project/Project/file.txt")
+	else if(file == "medium.txt")
+   	{
+		cout<<"Got request for medium file"<<endl;
+		infile.open("medium.txt");
+		frames2 = frameBuilder(infile);
+		frames = parityGetter(frames2);
+	}
+	else
    	{
 		cout<<"Got invalid filepath. Processing default..."<<endl;
-		infile.open("/home/sina3615/CPSC3780Project/Project/file.txt");
+		infile.open("file.txt");
 		frames2 = frameBuilder(infile);
 		frames = parityGetter(frames2);
 	}
@@ -189,7 +151,6 @@ string swapParity(string data)
 		temp = data.substr(0,data.size()-1);
 		temp.append("0");
 	}
-
 	return temp;
 }
 
@@ -251,6 +212,7 @@ vector<string> parityGetter(vector<string> f)
 				f[i].append("1");
 			}
 		}
+
 		//case for simulating bad parity
 		else
 		{
